8c79b46ead690836a559aa57505785a3b2e91c02
keemsora <73927510+keemsora@users.noreply.github.com>
2021-03-11 23:15:56 +0900

    회원 관리 예제

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence); //아이디 세팅
+        store.put(member.getId(), member); //저장소에 정보 저장
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); //loop를 돌면서 Map에서 찾아지면 반환, 없으면 Optional에 null이 포함되어 반환
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
